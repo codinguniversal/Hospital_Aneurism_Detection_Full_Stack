99a76413d79b0b0c4ce1deaa77c538c570da71e0
Moonbaboon3 <147054617+Moonbaboon3@users.noreply.github.com>
2026-06-22 22:27:25 +0300

    verify_password check fix

--- BackEnd/app/usecases/auth_use_cases/auth_user_use_case.py
+++ BackEnd/app/usecases/auth_use_cases/auth_user_use_case.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from app.services.security import verify_password
 from app.domain.repositories import UserRepository
 from app.domain.entities import UserEntity
 
@@ -13,10 +14,14 @@ class AuthenticateUserUseCase:
 
         user = await self.user_repo.get_by_identifier(clean_identifer)
 
-        if not user or user.password != password :
+        if not user:
+            return None
+
+        if not verify_password(password, user.password):
             return None
         
         return user
-
+        
+    
 
 
